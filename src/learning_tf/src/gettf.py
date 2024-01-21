d2bc7a0766058ef7e958ed5faa7eef35b5b64e7a
Jinliye <3211498596@qq.com>
2024-01-21 18:13:11 +0800

    multiple turtles in single file formation

--- src/learning_tf/src/gettf.py
+++ src/learning_tf/src/gettf.py
@@ -4,6 +4,7 @@ import math
 import tf
 import geometry_msgs.msg
 import turtlesim.srv
+import random
 
 if __name__ == '__main__':
     rospy.init_node('turtle_tf_listener')
@@ -14,27 +15,40 @@ if __name__ == '__main__':
     # 定义Service造一个海龟
     rospy.wait_for_service('spawn')
     spawner = rospy.ServiceProxy('spawn',turtlesim.srv.Spawn)
-    spawner(4,2,0,'turtle2')
+    name = rospy.get_param("~turtle")
+    spawner(4,2,0,name)
 
     # 定义发布海龟的topic
-    turtle_vel = rospy.Publisher('/turtle2/cmd_vel',geometry_msgs.msg.Twist,queue_size=1)
+    # turtle_vel = rospy.Publisher('/turtle2/cmd_vel',geometry_msgs.msg.Twist,queue_size=1)
+    turtle_vel = rospy.Publisher(f'/{name}/cmd_vel',geometry_msgs.msg.Twist,queue_size=1)
 
-    rate = rospy.Rate(10)
+    rate = rospy.Rate(20)
 
     while not rospy.is_shutdown():
         try:
-            listener.waitForTransform('/turtle2','/turtle1',rospy.Time(0),rospy.Duration(1))
+            listener.waitForTransform(f'/{name}','/turtle1',rospy.Time(0),rospy.Duration(1))
 
             # 查询tf树中/turtle1到/turtle2的变换（turtle1在turtle2下的位姿）
-            (trans,rot) = listener.lookupTransform('/turtle2','/turtle1',rospy.Time(0))
+            (trans,rot) = listener.lookupTransform(f'/{name}','/turtle1',rospy.Time(0))
             
         except (tf.LookupException,tf.ConnectivityException,tf.ExtrapolationException) as e:
             rospy.logwarn(f'error:{e}')
             continue
 
         # 用相对位置计算角速度，线速度
-        angular = 4*math.atan2(trans[1],trans[0])
-        liner = 0.5 * math.sqrt(trans[0] ** 2 + trans[1] ** 2)
+        roll,pitch,yaw = tf.transformations.euler_from_quaternion(rot)
+        desire_dis = rospy.get_param('~desire_dis')
+        temp = trans
+        temp[0] = trans[0]-desire_dis*math.cos(yaw)
+        temp[1] = trans[1]-desire_dis*math.sin(yaw)
+        angular = .7*math.atan2(temp[1],temp[0])
+
+        dis = math.sqrt(temp[0] ** 2 + temp[1] ** 2)
+        if dis > .05:
+            liner = .5*dis
+        else:
+            liner = 0
+            angular = .5*yaw
 
         # 发布速度控制指令 Topic
         cmd = geometry_msgs.msg.Twist()
