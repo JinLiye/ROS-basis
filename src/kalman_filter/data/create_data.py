74664059bd7e0a0cc5b1440489c69ef41d79663e
Jinliye <3211498596@qq.com>
2024-02-04 16:42:00 +0800

    create acceration data,and initial the struck

---
+++ src/kalman_filter/data/create_data.py
@@ -0,0 +1,12 @@
+import numpy as np
+from scipy.ndimage import gaussian_filter1d
+import matplotlib.pyplot as plt
+
+# 生成200个在[-2, 2]范围内的随机数据
+data = np.random.uniform(-2, 2, 200)
+
+# 高斯平滑
+smoothed_data = gaussian_filter1d(data, sigma=3)
+
+# 将平滑后的数据写入本地NumPy文件
+np.save('accelerations.npy', np.array(smoothed_data))
