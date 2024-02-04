6d8c7315f53dc91f66c3550a47babc4e061e6d25
Jinliye <3211498596@qq.com>
2024-02-04 23:36:21 +0800

    add parm_sim and gps

--- src/kalman_filter/src/gps.py
+++ src/kalman_filter/src/gps.py
@@ -1,1 +1,15 @@
 #! /usr/bin/env python
+import rospy
+import numpy as np
+from std_msgs.msg import Float64
+
+def getgps(data):
+    rospy.loginfo(f'gps:{data}')
+
+def main():
+    rospy.init_node('gps')
+    sub = rospy.Subscriber("position",Float64,getgps)
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
--- src/kalman_filter/src/parm_sim.py
+++ src/kalman_filter/src/parm_sim.py
@@ -1,1 +1,53 @@
 #! /usr/bin/env python
+import rospy
+import numpy as np
+from std_msgs.msg import Float64
+
+# 发布汽车的速度，位置，加速度
+class Car:
+    def __init__(self) -> None:
+        self.p = 0
+        self.v = 0
+
+    def step(self,a,dt,p_std_dev,v_std_dev):
+        self.p += self.v*dt + 0.5*a*dt**2
+        self.v += a*dt
+
+        self.p += np.random.normal(0,p_std_dev)
+        self.v += np.random.normal(0,v_std_dev)
+
+def main():
+    rospy.init_node("parm_sim")
+
+    pub1 = rospy.Publisher("position",Float64,queue_size=10)
+    pub2 = rospy.Publisher("velocity",Float64,queue_size=10)
+    pub3 = rospy.Publisher("acceleration",Float64,queue_size=10)
+
+    rate = rospy.Rate(10)
+    # filepath = f'../data/accelerations.npy'
+    
+    filepath = f'/home/qrobo/learn_ws/src/kalman_filter/data/accelerations.npy'
+    load_data = np.load(filepath)
+    i = 0
+    dt = 0.1
+    p_std_dev = 0.2
+    v_std_dev = 0.3
+    c = Car()
+
+    while (not rospy.is_shutdown) and i < 200 :
+        acc = load_data[i]
+        i += 1
+        c.step(acc,dt,p_std_dev,v_std_dev)
+        pub1.publish(c.p)
+        pub2.publish(c.v)
+        pub3.publish(acc)
+        rospy.loginfo(f'position:{c.p} a:{acc}')
+        rate.sleep()
+
+if __name__ == '__main__':
+    main()
+
+
+        
+
+
